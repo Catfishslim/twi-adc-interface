130f61d1078ed11cff982895342587e9b32efd20
Sean <newmansf@vcu.edu>
2017-11-16 02:57:50 -0500

    Revert "Merge branch 'trippsbranch'"
    This reverts commit 94221757cf102e86041aa61b9f25ae43df7d8ff9, reversing
    changes made to 0854028360397cf7cc5c780b147f6fafab87adae.

--- ADCsm.vhd
+++ ADCsm.vhd
@@ -1,11 +1,12 @@
 
-
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity adc_controller is
+entity ADC_controller is
+
+
 	port( clk     : in std_logic;
-				reset     : in std_logic;
+				rst     : in std_logic;
 				start   : in std_logic;
 				done_o  : in std_logic;
 				err_o   : in std_logic;
@@ -16,176 +17,129 @@ entity adc_controller is
 				a_i     : out std_logic_vector(7 downto 0);
 				d_i     : out std_logic_vector(7 downto 0);
 				data_out: out std_logic_vector(15 downto 0));
-end adc_controller;
+end ADC_controller;
 
 
-architecture behavior of ADC_controller is
+architecture Behavioral of ADC_controller is
 	
-	type state_type is (waiting, addressWrite, resetState, resetWait, configWrite, configWait, startRead, msgLow, msbRead, lsbRead);
+	type state_type is (idle, configWrite, readSig, msbRead, lsbRead);
 	signal present_state, next_state : state_type;
+	signal count_reset : std_logic;
 
-	constant addrAD2	 : STD_LOGIC_VECTOR(6 downto 0) := "0101000";	-- TWI address for the ADC
-  	constant writeCfg	 : STD_LOGIC_VECTOR(7 downto 0) := "00010000";	-- configuration register value for the ADC - read VIN0
-  	constant read_Bit  : STD_LOGIC := '1';
-  	constant write_Bit : STD_LOGIC := '0';
-	SIGNAL count_reset : std_logic;
-	SIGNAL count : integer := 0;
+	constant addrAD2	 : std_logic_vector(6 downto 0) := "0101000";	-- TWI address for the ADC
+  constant writeCfg	 : Std_logic_vector(7 downto 0) := "00010000";	-- configuration register value for the ADC - read VIN0
+  constant read_Bit  : std_logic := '1';
+  constant write_Bit : std_logic := '0';
 
+  constant config_wait : integer := 10;
+  constant srst_wait : integer := 2;
+  constant stb_wait : integer := 2;
+  constant bus_wait : integer := 1200;
+  constant msg_wait : integer := 510;
 
-
-  	procedure waitclocks(signal clock : std_logic;
-             	          N : INTEGER) is
-			begin
-				for lsbRead in 1 to N loop
-					wait until falling_edge(clock);	
-				end loop;
+  procedure waitclocks(signal clock : std_logic;
+                       N : integer) is
+		begin
+			for i in 1 to N loop
+				wait until falling_edge(clock);	
+			end loop;
 	end waitclocks;
 
   begin
 
-	-- Counts the number of clock edges
-	counter : PROCESS(clk)
-		BEGIN
-			IF(rising_edge(clk)) THEN
-				IF(count_reset='1') THEN 
-					count <= 0;
-				ELSE
-					count <= count + 1;
-				END IF;
-			END IF;  
-	END PROCESS counter;
-
-	clocked : process(clk, reset)
+	clocked : process(clk, rst)
 			begin
-	     if(reset='1') then 
-	       present_state <= waiting;
+	     if(rst='1') then 
+	       present_state <= idle;
 	    elsif(rising_edge(clk)) then
 	      present_state <= next_state;
 	    end if;  
 	 end process clocked;
 
-	nextStateDecode: process (start,done_o, present_state, count)
+	 counter : process(clk, count_reset)
+	 	begin
+	 		if(count_reset='1') then 
+       count <= 0;
+    	elsif(rising_edge(clk)) then
+      	count <= count + 1;
+    END IF;  
+
+	 	end process counter;
+
+	nextStateDecode: process (start,done_o)
 		begin
-		count_reset <= '0';
+			next_state <= present_state;	--default is to stay in current state
+
 			case(present_state) is
-				WHEN addressWrite =>
-					if(count < 10) then
-						next_state <= present_state;
-					else
-						next_state <= resetState;-- waitclocks(clk_sig, 10);
-						count_reset <= '1';-- reset counter		-- activate reset
+				when idle => 
+					if(rising_edge(start)) then --can't do rising edge
+						next_state <= configWrite;
 					end if;
-
-				WHEN resetState =>			
-					if(count < 2) then
-						next_state <= present_state;
-					else
-						next_state <= resetWait;--waitclocks(clk_sig, 2);
-						count_reset <= '1';-- reset counter
+				when configWrite =>
+					if(falling_edge(done_o)) then
+						next_state <= readSig;
 					end if;
-						
-				WHEN resetWait =>
-					if(count < 1200) then
-						next_state <= present_state;
-					else
-						next_state <= configWrite;			-- wait > 1000 clocks for bus to be "free"
-						count_reset <= '1';-- reset counter 
-					end if;
-					
-				WHEN configWrite =>
-					if(count < 2) then
-						next_state <= present_state;
-					else
-						next_state <= configWait;--waitclocks(clk_sig, 2);							-- two cycles for strobe to be captured
-						count_reset <= '1';-- reset counter
-					end if;
-				WHEN configWait =>
-					if(DONE_O'event and DONE_O='0') then -- wait until DONE_O_sig'event and DONE_O_sig='0';	-- wait until TWI controller signals done
-						next_state <= startRead;
-						count_reset <= '1';-- reset the counter
-					else
-						next_state <= present_state;
-					end if;
-				WHEN startRead =>
-					if(count < 2) then
-						next_state <= present_state;
-					else
-						next_state <= msgLow;-- waitclocks(clk_sig, 2);
-						count_reset <= '1';-- reset thec counter					-- two cycles for message to be captured			
-					end if;
-				WHEN msgLow =>			
-					if(DONE_O'event and DONE_O='0')then-- wait until DONE_O_sig'event and DONE_O_sig='0';	-- wait until TWI controller signals done
+				when readSig =>
+					if(falling_edge(done_o)) then
 						next_state <= msbRead;
-						count_reset <= '1';-- reset the counter					
-					else
-						next_state <= present_state;
 					end if;
-				WHEN msbRead =>
-					if(count > 510) then-- waitclocks(clk_sig, 510);						-- you have to go past 1/2 SCL cycle before dropping
+				when msbRead =>
+					if(falling_edge(done_o)) then
 						next_state <= lsbRead;
-						count_reset <= '1';
-					else
-						next_state <= present_state;
 					end if;
-				WHEN lsbRead =>
-					next_state <= waiting;
-					count_reset <= '1';-- reset the coutner?
-
-				WHEN waiting =>
-					if(start'EVENT AND start = '1') then
-						next_state <= addressWrite;
-						count_reset <= '1';
-					else
-						next_state <= present_state;
-					end if;
-		END CASE;
+				when lsbRead =>
+					next_state <= idle;	--might have to add a delay to ensure that the data is properly read
+			end case;
 	end process nextStateDecode;
 
 	outputDecode: process (present_state)
 		begin
-			count_reset <= '0';
 			case(present_state) is
-				WHEN addressWrite =>
-	    				MSG_I <= '0';					-- set signal default values
-	    				STB_I <= '0';					-- inactive
-					SRST <= '0';					-- inactive
-					A_I <= addrAD2 & write_Bit;		-- 0x50 address plus '0' for write
-					D_I <= writeCfg;				-- 0x10 configuration register (convert Vin0)
+				when idle =>
+					msg_i <= '0';
+					stb_i <= '0';
+					srst  <= '0';
+				when configWrite =>
+					msg_i <= '0';					-- set signal default values
+	   			stb_i <= '0';					-- inactive
+					srst <= '0';					-- inactive
+					a_i <= addrAD2 & write_Bit;		-- 0x50 address plus '0' for write
+					d_i <= writeCfg;				-- 0x10 configuration register (convert Vin0)
 					
-				WHEN resetState =>			
-					SRST <= '1';
-						
-				WHEN resetWait =>
-					SRST <= '0';
+					waitclocks(clk, 10);			-- activate reset
+					srst <= '1';
+					waitclocks(clk, 2);
+					srst <= '0';
 					
-				WHEN configWrite =>
-					STB_I <= '1';							-- start config write operation
-				
-				WHEN configWait =>
-					STB_I <= '0';
+					waitclocks(clk, 1200);			-- wait > 1000 clocks for bus to be "free"
+					
+					stb_i <= '1';								-- start config write operation
+					waitclocks(clk, 2);							-- two cycles for strobe to be captured
+					stb_i <= '0';
+				when readSig =>
+					a_i <= addrAD2 & read_Bit;					-- 0x50 address plus '1' for read		
+					msg_i <= '1';								-- signal multi-byte read
+					stb_i <= '1';								-- start read operation
+					waitclocks(clk, 2);							-- two cycles for message to be captured					
+					msg_i <= '0';								-- leave strobe high for multi-byte operation
+				when msbRead =>
+					waitclocks(clk, 510);       -- you have to go past 1/2 scl cycle before dropping
+					stb_i <= '0';               -- STB, I'm not sure why
+					data_out(15 downto 8) <= d_o; --load msb data read
+				when lsbRead =>
+					data_out(7 downto 0) <= d_o;
 
-				WHEN startRead =>
-					A_I <= addrAD2 & read_Bit;					-- 0x50 address plus '1' for read		
-					MSG_I <= '1';								-- signal multi-byte read
-					STB_I <= '1';								-- start read operation
-				
-				WHEN msgLow =>			
-					MSG_I <= '0';								-- leave strobe high for multi-byte operation
-
-				WHEN msbRead =>
-					STB_I <= '0';								-- STB, lsbRead'm not sure why
-					data_out(15 downto 8) <= D_O;				-- load MSB data read
-
-				WHEN lsbRead =>
-					data_out(7 downto 0) <= D_O;					-- load LSB data read
-
-				WHEN waiting =>
-					count_reset <= '1';-- reset the counter
 
 					
 			end case;
 	end process outputDecode;
 
-end behavior;
 
+
+
+
+
+
+
+end Behavioral;
 
